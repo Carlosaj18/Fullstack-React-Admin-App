2f609afa19c716080b8252fdbfd6c18bbdfc2538
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-15 18:14:35 -0500

    Inject muckup data to the databse

--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction
+} from "./data/index.js";
+
 
 /* CONFIGURATION */
 dotenv.config();
