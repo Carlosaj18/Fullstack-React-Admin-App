72c973f3eacfd9c5290a9f9c7d77a181171a47e2
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-15 18:15:43 -0500

    Created routers for /products, /customers and /transactions

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,5 +1,14 @@
 import express from "express";
+import {
+  getProducts,
+  getCustomers,
+  getTransactions, 
+} from "../controllers/client.js";
 
 const router = express.Router();
+
+router.get("/products", getProducts);
+router.get("/customers", getCustomers);
+router.get("/transactions", getTransactions);
 
 export default router;
