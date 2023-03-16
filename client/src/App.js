5b6d2548e7c8910c84d1820042c0551d0f79260a
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-15 20:00:31 -0500

    Implement daily, montly and breakdown scences

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,9 @@ import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
-
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -31,6 +33,9 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
