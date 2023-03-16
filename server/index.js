94463cc2722f0ac1d2bec848ef95d6eafef5131d
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-15 21:37:21 -0500

    Inject muck data for performance features

--- server/index.js
+++ server/index.js
@@ -16,13 +16,15 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
-  dataOverallStat
+  dataOverallStat,
+  dataAffiliateStat
 } from "./data/index.js";
 
 
